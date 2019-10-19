11aed42b5697ad1847ec671a1344d90fa164c7ad
manoj.satya <manoj.pesit@gmail.com>
2019-10-19 23:05:03 +0200

    Removed comments from Feedback.js

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -51,7 +51,6 @@ export default function Feedback({ firstPart, secondPart }) {
 
         <ButtonStyled type="submit">Submit</ButtonStyled>
       </form>
-      {/* <NavlinkStyled to="/reviews">See all reviews</NavlinkStyled> */}
     </WrapperStyled>
   );
 }
